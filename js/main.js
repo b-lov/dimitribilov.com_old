8f58e212c9434e8cffb1e7991254b16718704054
Max B <maxbilov@gmail.com>
2014-10-18 22:20:22 +0200

    put original website files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// JUSTIFIED GALLERY
+
+$(document).ready(function($) {
+
+	$("#gallery").justifiedGallery();
+	
+});
+
+
+// SWIPEBOX
+
+$(document).ready(function($) {
+
+	$('.swipebox').swipebox();
+
+} )( jQuery );
